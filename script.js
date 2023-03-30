e744128550e8aebe0ae4a1f45ffa3508207ec7ff
sinhauvong <sinhtntpd08642@fpt.edu.vn>
2023-03-30 11:45:38 +0700

    upload source code

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+//button kick mua hàng
 function addToCart(button) {
-    const productId = button.getAttribute('data-id');
-    location.href = `sanpham.html?id=${productId}`;
-  }
+  const productId = button.getAttribute('data-id');
+  location.href = `sanpham.html?id=${productId}`;
+}
 
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
 
 location.href = `muahang.html?id=${productId}&name=${productName}&price=${productPrice}`;
+
+
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+// end button kick mua hàng
+
+
+
+
 
--- script1.js
+++ script1.js
@@ -1,11 +1,9 @@
-const loginForm = document.getElementById("login-form");
-const registerForm = document.getElementById("register-form");
-
+//bắt đầu đk đn
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const username = document.getElementById("login-username").value;
   const password = document.getElementById("login-password").value;
-  // validate and log in user
+
 });
 
 registerForm.addEventListener("submit", (event) => {
@@ -13,5 +11,6 @@ registerForm.addEventListener("submit", (event) => {
   const username = document.getElementById("register-username").value;
   const email = document.getElementById("register-email").value;
   const password = document.getElementById("register-password").value;
-  // validate and register user
+
 });
+// end đk đn
